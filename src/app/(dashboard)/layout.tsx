da0dd60956a24bc81c2036ccf0b8f5eb47843ac6
Steven4Hooisma <s.hooisma@cleverzebra.nl>
2025-10-24 12:12:34 +0200

    Add dynamic breadcrumb

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -13,6 +13,7 @@ import {
     BreadcrumbList, BreadcrumbPage,
     BreadcrumbSeparator
 } from "@/components/ui/breadcrumb";
+import {AppHeader} from "@/components/app-header";
 
 
 export default function DashboardLayout({
@@ -24,28 +25,7 @@ export default function DashboardLayout({
         <SidebarProvider>
             <AppSidebar />
             <SidebarInset>
-                <header
-                    className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
-                    <div className="flex items-center gap-2 px-4">
-                        <SidebarTrigger className="-ml-1"/>
-                        <Separator
-                            orientation="vertical"
-                            className="mr-2 data-[orientation=vertical]:h-4"/>
-                        <Breadcrumb>
-                            <BreadcrumbList>
-                                <BreadcrumbItem className="hidden md:block">
-                                    <BreadcrumbLink href="#">
-                                        Building Your Application
-                                    </BreadcrumbLink>
-                                </BreadcrumbItem>
-                                <BreadcrumbSeparator className="hidden md:block"/>
-                                <BreadcrumbItem>
-                                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                                </BreadcrumbItem>
-                            </BreadcrumbList>
-                        </Breadcrumb>
-                    </div>
-                </header>
+                <AppHeader />
                 <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
                 {children}
                 </div>
---
+++ src/components/app-header.tsx
@@ -0,0 +1,57 @@
+"use client"
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+    BreadcrumbList,
+    BreadcrumbPage,
+    BreadcrumbSeparator
+} from "@/components/ui/breadcrumb";
+import {SidebarTrigger} from "@/components/ui/sidebar";
+import { Separator } from './ui/separator';
+import {usePathname} from "next/navigation";
+import React from "react";
+
+export function AppHeader() {
+    const pathname = usePathname();
+    const pathSegments = pathname.split('/').filter((segment) => segment);
+
+    return (
+        <>
+            <header
+                className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+                <div className="flex items-center gap-2 px-4">
+                    <SidebarTrigger className="-ml-1"/>
+                    <Separator orientation="vertical" className="mr-2 data-[orientation=vertical]:h-4"/>
+                    <Breadcrumb>
+                        <BreadcrumbList>
+                            <BreadcrumbItem className="hidden md:block">
+                                <BreadcrumbLink href="/">
+                                    Dashboard
+                                </BreadcrumbLink>
+                            </BreadcrumbItem>
+                            {pathSegments.map((segment, index) => {
+                                const href = `/${pathSegments.slice(0, index + 1).join('/')}`;
+                                const isLast = index === pathSegments.length - 1;
+
+                                return (
+                                    <React.Fragment key={href}>
+                                        <BreadcrumbSeparator />
+                                        <BreadcrumbItem>
+                                            {isLast ? (
+                                                <BreadcrumbPage>{segment}</BreadcrumbPage>
+                                            ) : (
+                                                <BreadcrumbLink href={href}>{segment}</BreadcrumbLink>
+                                            )}
+                                        </BreadcrumbItem>
+                                    </React.Fragment>
+                                );
+                            })}
+
+                        </BreadcrumbList>
+                    </Breadcrumb>
+                </div>
+            </header>
+        </>
+    )
+}
